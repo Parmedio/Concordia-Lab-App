d1c06c9070439b82520b550e1137db4781d91489
Alessandro Ferluga <alessandroferluga97@gmail.com>
2023-06-01 10:23:10 +0200

    Add Properties to ScientistModel.cs

--- ConcordiaLabApplication/PersistentLayer/Models/ScientistModel.cs
+++ ConcordiaLabApplication/PersistentLayer/Models/ScientistModel.cs
@@ -2,4 +2,10 @@
 
 public record ScientistModel
 {
+    public int Id { get; set; }
+    public string TrelloToken { get; set; } = null!;
+    public string TrelloMemberId { get; set; } = null!;
+    public string Name { get; set; } = null!;
+
+    public virtual IEnumerable<ExperimentModel> Experiments { get; set; }
 }
