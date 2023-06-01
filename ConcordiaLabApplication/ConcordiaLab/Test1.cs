46b9b74756769ad8f9e1b54043eaa217370bfa06
Alessandro Ferluga <alessandroferluga97@gmail.com>
2023-06-01 21:39:05 +0200

    Add Testing Unit

---
+++ ConcordiaLabApplication/ConcordiaLab/Test1.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLogic.APIConsumers.Abstract;
+using BusinessLogic.DTOs.TrelloDtos;
+
+using System.Text.Json;
+
+namespace ConcordiaLab;
+
+public class Test1
+{
+    private IApiReceiver _receiver;
+
+    public Test1(IApiReceiver receiver)
+    {
+        _receiver = receiver;
+    }
+
+    public void Run()
+    {
+        var a = JsonSerializer.Serialize<IEnumerable<TrelloExperimentDto>>(_receiver.GetAllExperimentsInToDoList().Result);
+        Console.WriteLine(a);
+    }
+}
