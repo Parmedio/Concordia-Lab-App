76471a90868ffcb1095d7f7b63ea3898298a5943
Parmedio <marco.da.pieve@gmail.com>
2023-06-19 16:34:48 +0200

    typo in migration

--- ConcordiaLabApplication/PersistentLayer/Migrations/20230615160625_Init.cs
+++ ConcordiaLabApplication/PersistentLayer/Migrations/20230615160625_Init.cs
@@ -167,8 +167,8 @@ namespace PersistentLayer.Migrations
                 values: new object[,]
                 {
                     { 1, "Alessandro Ferluga", "5bf9f901921c336b20b29d25", "ATTA5c0a0bf47c1be3f495ebb81c42316684ff55e1134be71c0eba2cbecdd0614558CDCC81F8" },
-                    { 2, "Marco de Piave", "639c692ed850f6055714fd55", "ATTAd93cf67ec0072d821ff32e199156a675ed9301feea0f899df160829b3f14082dAB1E41AD" },
-                    { 3, "Gabriele Ceccutti", "6474f28f0d4924c1eaff2824", "ATTA408bebeedb9948e62a1e38c11691049bc07e9329984c3897908a0127279faa4956E9CC86" }
+                    { 2, "Marco Da Pieve", "639c692ed850f6055714fd55", "ATTAd93cf67ec0072d821ff32e199156a675ed9301feea0f899df160829b3f14082dAB1E41AD" },
+                    { 3, "Gabriele Cecutti", "6474f28f0d4924c1eaff2824", "ATTA408bebeedb9948e62a1e38c11691049bc07e9329984c3897908a0127279faa4956E9CC86" }
                 });
 
             migrationBuilder.CreateIndex(
