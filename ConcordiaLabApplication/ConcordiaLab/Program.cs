10b171087ca7fe17b353fde27c1b8efa24d777eb
Alessandro Ferluga <alessandroferluga97@gmail.com>
2023-06-22 16:27:25 +0200

    Fix Dematerialization Bug

--- ConcordiaLabApplication/ConcordiaLab/Program.cs
+++ ConcordiaLabApplication/ConcordiaLab/Program.cs
@@ -1,22 +1,26 @@
 using AutoMapper;
 using AutoMapper.EquivalencyExpression;
+
 using BusinessLogic.APIConsumers.Abstract;
 using BusinessLogic.APIConsumers.Concrete;
 using BusinessLogic.APIConsumers.UriCreators;
 using BusinessLogic.AutomapperProfiles;
 using BusinessLogic.DataTransferLogic.Abstract;
 using BusinessLogic.DataTransferLogic.Concrete;
+
 using ConcordiaLab.AutomapperViewProfile;
+
 using Microsoft.EntityFrameworkCore;
+
 using PersistentLayer.Configurations;
 using PersistentLayer.Repositories.Abstract;
 using PersistentLayer.Repositories.Concrete;
+
 using Polly;
 using Polly.Contrib.WaitAndRetry;
 using Polly.Extensions.Http;
+
 using Quartz;
-using Scheduler;
-using Scheduler.Jobs;
 
 using ReportSender;
 using ReportSender.FileSystemManager.Abstract;
@@ -24,6 +28,9 @@ using ReportSender.FileSystemManager.Concrete;
 using ReportSender.Mail_Models;
 using ReportSender.MailSenderLogic.Abstract;
 using ReportSender.MailSenderLogic.Concrete;
+
+using Scheduler;
+using Scheduler.Jobs;
 
 namespace ConcordiaLab;
 
@@ -94,11 +101,10 @@ public class Program
 
             q.AddJob<MonthlyTriggerJob>(opts => opts.WithIdentity("MonthlyTriggerJob"))
                 .AddTrigger(opts => opts
-                    .WithIdentity("MonthlyTrigger")
                     .ForJob("MonthlyTriggerJob")
                     .StartNow()
                     .WithSimpleSchedule(builder =>
-                        builder.WithInterval(TimeSpan.FromDays(28))
+                        builder.WithInterval(TimeSpan.FromSeconds(5))
                             .RepeatForever()));
 
             q.AddJob<DataSynchronizerJob>(opts => opts.WithIdentity("DataSynchronizerJob"))
@@ -128,7 +134,7 @@ public class Program
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
-       app.Run();
+        app.Run();
     }
 
     static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
